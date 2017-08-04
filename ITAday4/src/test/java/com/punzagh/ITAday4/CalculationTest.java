1f445c6033708d3c18a269f479b6aee572afe198
PUNZAGH <PUNZAGH@ZHA-ITA027-W7.corp.oocl.com>
2017-08-04 13:45:36 +0800

    Merge Test

--- ITAday4/src/test/java/com/punzagh/ITAday4/CalculationTest.java
+++ ITAday4/src/test/java/com/punzagh/ITAday4/CalculationTest.java
@@ -6,7 +6,8 @@ public class CalculationTest {
 	@Test
 	public void test() {
 		Calculation calc = new Calculation();
-		System.out.println(calc.Addition(2, 2));
+		System.out.println(calc.Addition(2, 4));
+		//Mergedss
 		System.out.println(calc.Subtraction(2, 2));
 		System.out.println(calc.Multiplication(2, 2));
 		System.out.println(calc.Divition(2, 2));
